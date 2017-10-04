1196f10cf754f8cc228f358b7ba60a896563afb3
Magno Miranda Dantas <magnomirandadantas@MacBook-Pro-de-Magno.local>
2017-10-04 09:41:59 -0300

    Livro do cordova finalizado!

---
+++ Aplicações Mobile Híbridas com Cordova e PhoneGap/garconapp/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-splashscreen": "4.0.3",
+    "phonegap-plugin-barcodescanner": "6.0.8",
+    "cordova-plugin-statusbar": "2.2.3"
+};
+// BOTTOM OF METADATA
+});
